540730018630c7409095b269bc40d473af92a5df
NGK Sternhagen <sternhagen@protonmail.sh>
2016-12-09 16:07:26 -0800

    add packaging stuff

--- emamux-run-line.el
+++ emamux-run-line.el
@@ -1,3 +1,12 @@
+;;; emamux-run-line.el --- run the current line with emamux
+
+;;; Version: 0.0.0
+;;; Author: NGK Sternhagen <sternhagen@protonmail.ch>
+;;; Url: https://gitlab.com/skwuent/emamux-run-line.el
+;;; Keywords: tmux emamux
+;;; Created: 9 December 2016
+
+;;;###autoload
 (defun emamux-run-line ()
   "Marks whole line and does emamux:run-region"
   (interactive)
@@ -6,3 +15,7 @@
     (set-mark (line-end-position))
     (activate-mark)
     (call-interactively 'emamux:run-region)))
+
+(provide 'emamux-run-line)
+
+;;; emamux-run-line.el ends here
